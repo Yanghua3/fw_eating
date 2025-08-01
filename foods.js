dbf9d9b816fcd7d869b7421f677995254adbc43b
root <167655463@qq.com>
2025-08-01 17:27:07 +0800

    [fix] 修改数据结构

---
+++ foods.js
@@ -0,0 +1,32 @@
+// 食物数据 - 可以根据需要修改
+const foods = [
+  {
+    name: "汉堡",
+    icon: "fa-hamburger",
+    description: "经典塔斯汀汉堡配可乐薯条",
+  },
+  { name: "披萨", icon: "fa-pizza-slice", description: "S-pizza披萨速递" },
+  { name: "食堂", icon: "fa-apple-whole", description: "最次的选择" },
+  { name: "牛肉面", icon: "fa-utensils", description: "劲道拉面配大块牛肉" },
+  { name: "炸鸡", icon: "fa-drumstick-bite", description: "香脆多汁炸鸡" },
+  { name: "烤肉饭", icon: "fa-bacon", description: "张姐秘制烤肉配米饭" },
+  { name: "饺子", icon: "fa-utensils", description: "斯哈袁记云饺" },
+  { name: "盖浇饭", icon: "fa-utensils", description: "小叶面馆多种口味可选" },
+  { name: "西米露", icon: "fa-utensils", description: "一中西米露嘎嘎凉快" },
+  {
+    name: "麻抄手",
+    icon: "fa-hand-holding",
+    description: "鲜美可口的老麻抄手",
+  },
+  { name: "炒菜", icon: "fa-carrot", description: "年轻20年·绝无预制菜" },
+  { name: "米线", icon: "fa-utensils", description: "经典米线" },
+  {
+    name: "麻辣烫",
+    icon: "fa-fire-flame-curved",
+    description: "自选食材麻辣烫",
+  },
+  { name: "蜜雪冰城", icon: "fa-ice-cream", description: "蜜雪冰城甜蜜蜜" },
+  { name: "饼干", icon: "fa-cookie", description: "干巴巴" },
+  { name: "酒", icon: "fa-champagne-glasses", description: "小酌一杯" },
+  { name: "小蛋糕", icon: "fa-cake-candles", description: "甜品" },
+];
